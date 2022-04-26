42ccb8df168f5f291e8e1f64f4905d30f1c47af7
masaxsuzu <masaxsuzu+github@gmail.com>
2022-04-26 21:38:33 +0900

    Fix format

--- src/lib/deck/HandSimulator_Regression.spec.ts
+++ src/lib/deck/HandSimulator_Regression.spec.ts
@@ -2,9 +2,7 @@ import { Deck } from "./Deck";
 import { HandSimulator } from "./HandSimulator";
 
 test("R1", () => {
-  let cards = [
-    { name: "x", number: 1 },
-  ];
+  let cards = [{ name: "x", number: 1 }];
   let deck = new Deck(5, cards);
   let sim = new HandSimulator(deck);
   let got = sim.compute(2, `"x" < 1`);
